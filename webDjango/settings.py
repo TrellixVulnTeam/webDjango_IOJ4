c860704e3063b6c69fcb3402ae9a757c91431af0
qiaoyu <qiaoyu19931002@163.com>
2021-07-01 20:52:16 +0800

    this is a new start for the establishment of a website !!!

--- webDjango/settings.py
+++ webDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=rp@i*ajh(r7z01g4*m5nx0yq&ndmeq9-$6a3^e@am^w&bo%y1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
